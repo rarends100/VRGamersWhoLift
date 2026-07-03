f504b7a2cb9a61435b8b466725f401350adc461f
Rob 25 <robert.arends100@gmail.com>
2026-07-03 11:38:33 -0500

    * Added the ProfileController * Changed it so that now the Profile controller is responsible for the profile page * Added new View dir \Profile

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VRGamersWhoLift.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Profile()
+        {
+
+
+            return View();
+        }
+    }
+}
