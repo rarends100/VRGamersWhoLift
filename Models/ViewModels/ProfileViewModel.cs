c38a96a9fb3d989ef3cc5d1839978c0e97562e4a
Rob 25 <robert.arends100@gmail.com>
2026-07-13 17:03:08 -0500

    - Added functionality to the HelperFunctionsMisc.PopulateProfileData() function so that it now also sets a default image when there is no profile image selected yet by the user - Fixed bug where if you logged in the PopulateProfileData() function did not descriminate between users in the database lookup of the profile image or the profile banner. Added another WHERE clause to the conditions for the UserId.

--- Models/ViewModels/ProfileViewModel.cs
+++ Models/ViewModels/ProfileViewModel.cs
@@ -6,9 +6,10 @@
         public string Picture { get; set; } = null!; //profile picture
         public string Banner { get; set; } = null!;
         public string UserName { get; set; } = null!;
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string UserId { get; set; } = null!;
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public List<string> Gallery { get; set; } = null!; //pictures in the gallery for the specified user
         public List<string> Errors { get; set; } = null!;
 
