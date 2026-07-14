4bc261550f2d0b1ba19e6f45a0f5096c15965601
Rob 25 <robert.arends100@gmail.com>
2026-07-13 20:13:37 -0500

    - Added PostController -> it will control adding, updating/editing, and deleting posts -> each time routing back to the appropriate profile page (the logged in user) - working on razor pages and then will come back and create these methods

---
+++ Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VRGamersWhoLift.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult AddPost()
+        {
+            //TODO write methods
+            return View("Profile");
+        }
+
+        public IActionResult DeletePost()
+        {
+            return View("Profile");
+        }
+
+        public IActionResult UpdatePost()
+        {
+            return View("Profile");
+        }
+
+
+    }
+}
