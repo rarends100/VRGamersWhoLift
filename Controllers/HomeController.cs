6b0f16520207e91582fed2ea34cc46ac558a6657
Rob 25 <robert.arends100@gmail.com>
2026-07-29 21:13:52 -0500

    - HomeController tests passed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,12 +6,6 @@ namespace VRGamersWhoLift.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
         public IActionResult Index()
