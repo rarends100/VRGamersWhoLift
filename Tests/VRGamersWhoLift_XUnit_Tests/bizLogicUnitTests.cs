92e026a344ad8fba651e128c9e7a49ce2cd41954
Rob 25 <robert.arends100@gmail.com>
2026-07-31 12:28:34 -0500

    - Current Testing needs complete — will continue writing tests as I develop as needed going forward (i.e. Testing ONLY on biz logic)

---
+++ Tests/VRGamersWhoLift_XUnit_Tests/bizLogicUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using VRGamersWhoLift.Helpers;
+using VRGamersWhoLift.Models;
+using VRGamersWhoLift.Models.Abstract;
+using VRGamersWhoLift.Models.database;
+
+namespace VRGamersWhoLift_XUnit_Tests
+{
+    public class bizLogicUnitTests
+    {
+       [Fact]
+        public void RegisterActive_unitTest_isString ()
+        {
+            //Arrange
+            string link = "/link";
+            string currentAction = "TEST";
+
+            //Act
+            string result = Nav.RegisterActive(link, currentAction);
+
+            //Assert
+            Assert.IsType<String>(result);
+        }
+
+        [Fact]
+        public void RegisterActive_unitTest_isConverted()
+        {
+            //Arrange
+            string link = "/link";
+            string currentAction = "TEST";
+
+            //Act
+            string result = Nav.RegisterActive(link, currentAction);
+
+            //Assert
+            Assert.NotEqual(currentAction, result);
+        }
+
+    }
+}
